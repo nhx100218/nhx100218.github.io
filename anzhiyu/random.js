241ae85afef9d59ddbf31886e6e8706884f0ad8f
nhx100218 <nhx100218@163.com>
2026-02-13 16:21:28 +0800

    Site updated: 2026-02-13 16:21:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/06/zkandby/","2026/02/13/hello-world/","2025/12/21/This program cannot be run in DOS mode./","2025/06/19/2/","2025/06/14/zkzk/","2025/06/08/Level ML-33 只是钢管大厦而已/","2025/06/04/n3/","2025/06/04/n2/","2025/05/18/xiao/","2025/05/02/2025/","2025/05/01/汪五一生日快乐/"];function toRandomPost(){
+var posts=["2026/02/13/pg11/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
