51c3ccacf9bb9d267f1827299765b395ed867704
nhx100218 <nhx100218@163.com>
2026-03-15 12:26:08 +0800

    Site updated: 2026-03-15 12:26:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/15/office/","2026/02/13/pg11/"];function toRandomPost(){
+var posts=["2026/03/15/otp/","2026/03/15/office/","2026/02/13/pg11/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
